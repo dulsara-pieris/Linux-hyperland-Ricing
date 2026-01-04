18003fa178f544f918d7cd1abc5a603d4f8a8d3f
Dulsara Pieris <dulsara.synapsnex@gmail.com>
2026-01-04 17:50:01 +0530

    Delete kitty directory

--- kitty/cursor-pulse.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-while true; do
-    kitty @ set-cursor-color "#00F0FF"   # neon cyan
-    sleep 0.45
-    kitty @ set-cursor-color "#FF00E5"   # neon magenta
-    sleep 0.45
-done
--- kitty/padding-zoom.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-for p in 0 4 8 12 16 20; do
-    kitty @ set-window-padding $p $(( p/2 ))
-    sleep 0.05
-done
